dd2f1029d65bc315001b18692b0810eb09511868
Roman Lomov <romario.lomov@gmail.com>
2024-08-01 14:16:07 +0600

    Add User Controller, test Thymeleaf

--- src/main/java/springmvc_hiber/controller/UserController.java
+++ src/main/java/springmvc_hiber/controller/UserController.java
@@ -1,15 +1,26 @@
 package springmvc_hiber.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springmvc_hiber.service.UserService;
 
 @Controller
 @RequestMapping("/")
 public class UserController {
+    UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping()
-    public String test() {
+    public String test(Model model) {
+
+        model.addAttribute("user", userService.listUsers().get(0));
         return "test";
     }
 }
